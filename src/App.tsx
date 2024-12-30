08b314e275b2f07d281427ffa4bddd2f1afed5b5
Kaguenoakuma <shadowfocker666@gmail.com>
2024-12-30 12:23:18 -0600

    Proyecto inicial Calculadora de propinas

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <header className="bg-teal-400 py-4">
+        <h1 className="text-center text-4xl font-black"></h1>
+      </header>
+    </>
+  )
+}
+
+export default App
